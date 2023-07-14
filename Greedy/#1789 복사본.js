701e1a8b46e14d277ffd4a3f3199bb1bfe2813c1
leehyelim0692 <qazxcv0691@naver.com>
2023-07-14 16:57:39 +0900

    #1789: 수들의 합

--- Greedy/#1789 복사본.js
+++ Greedy/#1789 복사본.js
@@ -3,16 +3,12 @@ const _input = require("fs").readFileSync("/dev/stdin").toString().trim();
 function solution(input) {
     let sum = 0;
     let i = 0;
-    let answer = 0;
 
-    for (i = 0; i <= +input; i++) {
+    for (i = 0; i < +input; i++) {
         sum += i;
-        if (sum > +input) {
-            answer = i - 1;
-            break;
-        }
+        if (sum > +input) break;
     }
-    return answer;
+    return i - 1;
 }
 
 console.log(solution(_input));
