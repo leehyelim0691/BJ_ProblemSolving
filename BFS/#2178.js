41dfc0c8bef7d7c7dd70e4fc108c86471261be50
leehyelim0692 <qazxcv0691@naver.com>
2023-07-19 17:16:15 +0900

    #2178: 미로 탐색

---
+++ BFS/#2178.js
@@ -0,0 +1,44 @@
+const _input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+function solution(input) {
+    const [N, M] = input
+        .shift()
+        .split(" ")
+        .map((item) => +item);
+    const graph = [];
+    const dy = [0, 0, 1, -1];
+    const dx = [1, -1, 0, 0];
+    let answer = 0;
+
+    input.map((item, index) => {
+        const row = item.split("").map((item) => +item);
+        graph.push(row);
+    });
+
+    const visited = Array.from(Array(N), () => Array(M).fill(0)); // 행렬
+    const needVisit = []; // [[a, b], [c, d]]
+
+    needVisit.push([0, 0]);
+    visited[0][0] = 1;
+
+    while (needVisit.length) {
+        const size = needVisit.length;
+        for (let j = 0; j < size; j++) {
+            const [ny, nx] = needVisit.shift();
+
+            for (let i = 0; i < dy.length; i++) {
+                const y = ny + dy[i];
+                const x = nx + dx[i];
+
+                if (y >= 0 && y < N && x >= 0 && x < M && !visited[y][x] && graph[y][x] === 1) {
+                    needVisit.push([y, x]);
+                    visited[y][x] = 1;
+                }
+            }
+        }
+        answer++;
+    }
+    return answer;
+}
+
+console.log(solution(_input));
